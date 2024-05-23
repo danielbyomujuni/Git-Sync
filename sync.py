0e69ce6be28b4771bc6b561fe6ff7b2d97b96ca6
danielb <danielbyomujuni@nekosyndicate.com>
2024-05-23 20:17:20 +0000

    disable branch sync for right now

--- sync.py
+++ sync.py
@@ -28,7 +28,9 @@ def read_config():
 
 def update(repo):
     os.chdir(f"./{repo.name}")
-    os.system("git branch -r | grep -v '\\->' | sed \"s,\\x1B\\[[0-9;]*[a-zA-Z],,g\" | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done")
+
+    print("Verifying branches")
+    #os.system("git branch -r | grep -v '\\->' | sed \"s,\\x1B\\[[0-9;]*[a-zA-Z],,g\" | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\" &> /dev/null; done")
     os.system(f"git pull --all")
     os.system("git push down --tags")
     os.system("git push down --all")
@@ -38,7 +40,8 @@ def update(repo):
 def create(repo):
     os.system(f"git clone {repo.up_stream} ./{repo.name}")
     os.chdir(f"./{repo.name}")
-    os.system("git branch -r | grep -v '\\->' | sed \"s,\\x1B\\[[0-9;]*[a-zA-Z],,g\" | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done")
+    #os.system("git branch -r | grep -v '\\->' | sed \"s,\\x1B\\[[0-9;]*[a-zA-Z],,g\" | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done")
+
     os.system(f"git remote add down {repo.down_stream}")
     os.system("git push down --tags")
     os.system("git push down -all")
